8d9c6c99be53c909da5fb48aabcd153759cb5d17
Jean Claveau <jean.claveau@gmail.com>
2022-01-28 21:45:22 +0100

    Fix nanoid usage

--- src/common/uid-generator.ts
+++ src/common/uid-generator.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import * as nanoid from 'nanoid';
 
+// Your alphabet set
+const alphabet = '0123456789ABCD';
+
+// generator is a function that returns a random string
+// of length 10, with alphabets from the characters in `alphabet` constant
+const generator = nanoid.customAlphabet(alphabet, 10);
 @Injectable()
 export class UidGenerator {
     generate(): string {
-        return nanoid();
+        return generator();
     }
 }
